612e4c2deae89385ade11a420462d1ed08f10fd6
Imanol Eizaguirre <imanol@imanol.cc>
2013-06-07 13:07:29 +0200

    configuration file

---
+++ configuration.py
@@ -0,0 +1,8 @@
+# URLs
+
+api_url = "https://marketplace.firefox.com%s"
+validation_url = api_url % "/api/v1/apps/validation/"
+login_url = api_url % "/api/v1/account/login/"
+submission_url = api_url % "/api/v1/apps/app/"
+
+max_retries = 3
